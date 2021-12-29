6837a4e4c9e702d85b85f30a0805e2cada3bff91
Wt colab <93501803+hungwt@users.noreply.github.com>
2021-12-29 19:42:11 +0700

    Create installbad.sh

---
+++ installbad.sh
@@ -0,0 +1,6 @@
+
+echo "installing the Badlion Client..."
+cp BadlionClient /usr/bin/
+cp badlion.desktop /usr/share/applications
+chmod a+x /usr/bin/BadlionClient
+chmod a+x /usr/share/applications/badlion.desktop
