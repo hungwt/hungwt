08473d1529886bf2952171cdebb5c7be4f18cfc6
Wt colab <93501803+hungwt@users.noreply.github.com>
2022-01-13 12:25:02 +0700

    Update and rename installbad.sh to installdis.sh

--- installbad.sh
+++
@@ -1,7 +0,0 @@
-
-echo "installing the Badlion Client..."
-cp BadlionClient /usr/bin/
-cp badlion.desktop /usr/share/applications
-chmod a+x /usr/bin/BadlionClient
-chmod a+x /usr/share/applications/badlion.desktop
-echo badlion ok of run
