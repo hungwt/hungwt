93fe167f31a85d08007585eda7cab40ca8033955
Wt colab <93501803+hungwt@users.noreply.github.com>
2022-01-13 12:31:20 +0700

    Update installdis.sh

--- installdis.sh
+++ installdis.sh
@@ -1,4 +1,5 @@
 wget https://download846.mediafire.com/g0no2neb5jfg/8hc9kjo42n1iysy/discord.deb
 sudo apt update && sudo apt upgrade -y && sudo apt get update && clear
+sudo apt install gdebi -y
 sudo apt install /discord.deb
 clear
